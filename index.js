914a02bc54a511635315944de4c0d6294507a532
proyetei <75600096+proyetei@users.noreply.github.com>
2022-09-02 17:19:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const rwClient = require("./twitterClient.js");
-
+const CronJob = require("cron").CronJob;
+//runs a promise
 const tweet = async () => {
     try{
-        await rwClient.v1.tweet(status: "")
+        //tweet message
+        await rwClient.v1.tweet(status: "Movie information")
     }
     catch(e){
         console.error(e)
     }
 }
 
+//the time that this bot is going to post
+const job = new CronJob("0 5 * * *", () => {
+    tweet()
+})
+
+job.start();
