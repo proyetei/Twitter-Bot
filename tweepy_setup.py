1c1abfd793f4f83d435a70988cfc478c6c646a16
MohammadMohsinKhan <khanm349@mcmaster.ca>
2022-09-03 17:01:33 -0400

    Added basic functionality

--- tweepy_setup.py
+++ tweepy_setup.py
@@ -19,4 +19,21 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text="Hellow Twitter")
+#How to create a tweet
+#client.create_tweet(text="Hellow Twitter")
+
+#How to like a tweet
+#client.like(<The tweet ID>) the tweet ID is the bunch of numbers in the twitter post link
+
+#How to retweet a tweet
+#client.retweet(<The tweet ID>) the tweet ID is the bunch of numbers in the twitter post link
+
+#How to reply to a tweet
+#client.create_tweet(in_reply_to_tweet_id=<The tweet ID>, text = "This is the what reply would be")
+
+#How to scan for tweets in our timeline
+
+for tweet in api.home_timeline():
+    print(tweet.text)
+
+
