b4711854f46b376262bd1f8b8003f848d49225eb
MohammadMohsinKhan <khanm349@mcmaster.ca>
2022-09-08 20:50:37 -0400

    Added tutorial code on how to scan all tweets

--- tweepy_setup.py
+++ tweepy_setup.py
@@ -1,10 +1,9 @@
 import tweepy
 import json
+import time
 
 f = open("twitterKeys.json")
 data = json.load(f)
-
-print(data["keys"]["API_Key"])
 
 api_key = data["keys"]["API_Key"]
 api_secret = data["keys"]["API_Key_Secret"]
@@ -23,7 +22,7 @@ api = tweepy.API(auth)
 #client.create_tweet(text="Hellow Twitter")
 
 #How to like a tweet
-#client.like(<The tweet ID>) the tweet ID is the bunch of numbers in the twitter post link
+#client.like(<The Tweet ID>)
 
 #How to retweet a tweet
 #client.retweet(<The tweet ID>) the tweet ID is the bunch of numbers in the twitter post link
@@ -32,8 +31,38 @@ api = tweepy.API(auth)
 #client.create_tweet(in_reply_to_tweet_id=<The tweet ID>, text = "This is the what reply would be")
 
 #How to scan for tweets in our timeline
-
+'''
 for tweet in api.home_timeline():
     print(tweet.text)
+'''
 
+#How to get a certain users id
+#person = client.get_user(username="Mohsinxn1").data.id
 
+#get a users tweets
+'''
+for tweet in client.get_users_tweets(person).data:
+    print(tweet.text)
+'''
+
+#How to scan all tweets and print a tweet with a keyword
+'''
+searchTerms = ["Python"]
+
+class MyStream(tweepy.StreamingClient):
+    def on_connect(self):
+        print("Connected")
+
+    def on_tweet(self, tweet):
+        if tweet.referenced_tweets == None:
+            print(tweet.text)
+
+            time.sleep(0.2)
+
+stream = MyStream(bearer_token)
+
+for term in searchTerms:
+    stream.add_rules(tweepy.StreamRule(term))
+
+stream.filter(tweet_fields=["referenced_tweets"])
+'''
